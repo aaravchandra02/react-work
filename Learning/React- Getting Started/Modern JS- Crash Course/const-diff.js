a17a65f1c16f291ca4f73986f5ba800c32fe3a93
Aarav Chandra <aaravchandra02@gmail.com>
2020-07-26 02:03:48 -0700

    initial commit

---
+++ Learning/React- Getting Started/Modern JS- Crash Course/const-diff.js
@@ -0,0 +1,10 @@
+// Scalar values - immutable
+const answer = 42;
+const greeting = 'Hello';
+
+// Arrays and Objects - the position where its pointed is immutable but the values are mutable. 
+const numbers = [2, 4, 6];
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+};
