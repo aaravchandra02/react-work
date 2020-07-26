d6596eba24f379fc50722ac500081c32ddcb10b2
Aarav Chandra <aaravchandra02@gmail.com>
2020-07-26 00:14:01 -0700

    Individual function added

--- Learning/React- Getting Started/The Basics/hello-world.jsx
+++ Learning/React- Getting Started/The Basics/hello-world.jsx
@@ -166,4 +166,35 @@ ReactDOM.render(
 )
 
 // Component Reusability
+function Button(props) {
+    return (
+        <button onClick={() => props.onClickFunction(props.increment)}>
+            +{props.increment}
+        </button>
+    );
+}
 
+function Display(props) {
+    return (
+        <div>{props.message}</div>
+    );
+}
+
+function App() {
+    const [counter, setCounter] = useState(0);
+    const incrementCounter = (i_v) => setCounter(counter + i_v);
+    return (
+        <div>
+            <Button onClickFunction={incrementCounter} increment={1} />
+            <Button onClickFunction={incrementCounter} increment={5} />
+            <Button onClickFunction={incrementCounter} increment={10} />
+            <Button onClickFunction={incrementCounter} increment={100} />
+            <Display message={counter} />
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('mountNode'),
+);
