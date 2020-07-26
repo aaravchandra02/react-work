96953834c37897d13a14d53ff2fca21aa9ad6ed7
Aarav Chandra <aaravchandra02@gmail.com>
2020-07-26 00:18:31 -0700

    added handleClick

--- Learning/React- Getting Started/The Basics/hello-world.jsx
+++ Learning/React- Getting Started/The Basics/hello-world.jsx
@@ -198,3 +198,39 @@ ReactDOM.render(
     <App />,
     document.getElementById('mountNode'),
 );
+
+// More readble code
+
+function Button(props) {
+    const handleClick = () => props.onClickFunction(props.increment)
+    return (
+        <button onClick={handleClick}>
+            +{props.increment}
+        </button>
+    );
+}
+
+function Display(props) {
+    return (
+        <div>{props.message}</div>
+    );
+}
+
+function App() {
+    const [counter, setCounter] = useState(0);
+    const incrementCounter = (i_v) => setCounter(counter + i_v);
+    return (
+        <div>
+            <Button onClickFunction={incrementCounter} increment={1} />
+            <Button onClickFunction={incrementCounter} increment={5} />
+            <Button onClickFunction={incrementCounter} increment={10} />
+            <Button onClickFunction={incrementCounter} increment={100} />
+            <Display message={counter} />
+        </div>
+    );
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('mountNode'),
+);
