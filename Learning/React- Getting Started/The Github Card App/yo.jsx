1b6462c8eff39dd12766397d12107bbe2f2c6b2e
Aarav Chandra <aaravchandra02@gmail.com>
2020-07-27 14:08:32 -0700

    added Card placeholder

--- Learning/React- Getting Started/The Github Card App/yo.jsx
+++ Learning/React- Getting Started/The Github Card App/yo.jsx
@@ -7,8 +7,12 @@
 class Card extends React.Component {
     render() {
         return (
-            <div className='github-profile'>
-                A Github Profile
+            <div className="github-profile">
+                <img src="https://placehold.it/75" />
+                <div className="info">
+                    <div className="name">Name here...</div>
+                    <div className="company">Company here...</div>
+                </div>
             </div>
         );
     }
