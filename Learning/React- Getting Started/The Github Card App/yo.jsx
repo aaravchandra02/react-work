cb8c78620eeb227a019f551e596d3d8482bbbd32
Aarav Chandra <aaravchandra02@gmail.com>
2020-07-27 14:07:31 -0700

    added class components

--- Learning/React- Getting Started/The Github Card App/yo.jsx
+++ Learning/React- Getting Started/The Github Card App/yo.jsx
@@ -1,8 +1,37 @@
 //class component
 
-const App = ({ title }) => (
-    <div className="header">{title}</div>
-);
+//what component ot create?
+// card
+// list of cards
+
+class Card extends React.Component {
+    render() {
+        return (
+            <div className='github-profile'>
+                A Github Profile
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    // constructor
+
+    // its required in a class component, it return the virtual dom decription of our component
+    render() {
+        return (
+            <div>
+                <div className="header">{this.props.title}</div>
+                <Card />
+            </div>
+        );
+    }
+}
+
+// function component
+// const App = ({ title }) => (
+//     <div className="header">{title}</div>
+// );
 
 ReactDOM.render(
     <App title="The GitHub Cards App" />,
