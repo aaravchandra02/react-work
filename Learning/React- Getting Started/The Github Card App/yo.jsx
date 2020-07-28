9dd50d29e338457e9f15b56678b20c5ef76a27ab
Aarav Chandra <aaravchandra02@gmail.com>
2020-07-27 17:56:19 -0700

    added multple card and working

--- Learning/React- Getting Started/The Github Card App/yo.jsx
+++ Learning/React- Getting Started/The Github Card App/yo.jsx
@@ -5,11 +5,67 @@
 // list of cards
 // for styling either use global styling using CSS or use JS styling as shown below.
 
+
+
+const testData = [
+    {
+        "name": "Aarav Chandra",
+        "avatar_url": "https://avatars3.githubusercontent.com/u/42168454?v=4",
+        "location": "United States"
+    },
+    {
+        "name": "Fabien Potencier",
+        "avatar_url": "https://avatars3.githubusercontent.com/u/47313?v=4",
+        "location": "Lille, France",
+    },
+    {
+        "name": "Michele Bertoli",
+        "avatar_url": "https://avatars2.githubusercontent.com/u/1308971?v=4",
+        "location": "London, England",
+    }
+]
+
+
+
+// class Card extends React.Component {
+//     render() {
+//         return (
+//             <div className="github-profile">
+//                 <img src="https://placehold.it/85" />
+//                 <div className="info">
+//                     <div className="name">Name here...</div>
+//                     <div className="company">Company here...</div>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+const CardList = (props) => {
+    return (
+        <div>
+            {/* 'map' is a JS fn which takes Fn as an argument and 
+            applies it to every element of the array to return one 
+            array to another array sing this fn to convert evry value.
+            profile element is gained from map which is then expanded and then passed.
+            Its converted into :
+            [<Card />,<Card />,<Card />]
+            which is ultimately:
+            [React.createElement(),React.createElement(),React.createElement()]
+            */}
+            {testData.map(profile => <Card {...profile} />)}
+        </div>
+    );
+
+};
+
 class Card extends React.Component {
     render() {
+        // profile is a local variable
+        const profile = this.props;
         return (
             <div className="github-profile">
-                <img src="https://placehold.it/85" />
+                <img src={profile.avatar_url} />
                 <div className="info">
                     <div className="name">Name here...</div>
                     <div className="company">Company here...</div>
@@ -22,12 +78,12 @@ class Card extends React.Component {
 class App extends React.Component {
     // constructor
 
-    // its required in a class component, it return the virtual dom decription of our component
+    // its required in a class component, it return the virtual DOM decription of our component
     render() {
         return (
             <div>
                 <div className="header">{this.props.title}</div>
-                <Card />
+                <CardList />
             </div>
         );
     }
